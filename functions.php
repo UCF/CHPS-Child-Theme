16d4a805160639a904f973017ddb9751b0720ab0
cohpa <djanosik@ucf.edu>
2018-05-16 14:23:41 -0400

    revert

--- functions.php
+++ functions.php
@@ -1,6 +1,25 @@
 
 <?php // ADDED BY DAVID JANOSIK
+include 'shortcodes.php';
 
+add_action( 'wp_enqueue_scripts', 'my_add_stylesheet' );
+function my_add_stylesheet() {
+    wp_enqueue_style( 'my-style', get_stylesheet_directory_uri() . '/css/custom.css?'.rand(), false, 'all' );
+}
+
+
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'Colleges-Theme-style'; // This is 'Colleges-Theme-style' for the Colleges-Theme.
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/static/css/style.min.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 
 
