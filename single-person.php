dbfc10b2cc8b0df1a18309e1dc14a07340499b03
cohpa <djanosik@ucf.edu>
2018-05-23 10:09:58 -0400

    tweak

--- single-person.php
+++ single-person.php
@@ -272,12 +272,12 @@ $posts = get_posts(array(
 									
 									<?php if( !empty($repeater_subfield_name) ): ?>
 									<h5>Affiliations</h5>
-									<?php endif; ?>
 										<ul>
 										<?php while (have_rows('affiliations') ): the_row(); ?> 
 												<li><?php if(get_sub_field('aff_url')) { ?><a href="<?php the_sub_field('aff_url'); ?>" title="<?php the_sub_field('aff_name'); ?>" target="_blank"><?php the_sub_field('aff_name'); ?></a><?php }	else {the_sub_field('aff_name');}?></li>
 										 <?php endwhile; ?>	
 										 </ul>
+									<?php endif; ?>
 								<?php endif; ?>
 							</div>
 						</div>
