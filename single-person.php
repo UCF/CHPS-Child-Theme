1aad22244aeaa3469914f0b272e083d56fc4c030
cohpa <djanosik@ucf.edu>
2018-05-23 13:07:01 -0400

    revert

--- single-person.php
+++ single-person.php
@@ -266,55 +266,14 @@ $posts = get_posts(array(
 									 </div>
 								<?php }?>
 
-									
-									
-									
-										<?php if (have_rows('affiliations') ) { ?>
-											<h5>Affiliations</h5>
-												<ul>
-												<?php while (have_rows('affiliations') ): the_row(); ?> 
-													<?php $affiliation_name = get_sub_field('aff_name');
-													if( !empty($affiliation_name) ): ?>
-													
-														<li><?php if(get_sub_field('aff_url')) { ?><a href="<?php the_sub_field('aff_url'); ?>" title="<?php the_sub_field('aff_name'); ?>" target="_blank"><?php the_sub_field('aff_name'); ?></a><?php }	else {the_sub_field('aff_name');}?></li>
-														
-												 	<?php endif; ?>
-												 
-												 <?php endwhile; ?>	
-												 </ul>
-										<?php }?>
-										
-
-						
-
-								
-<?php if (have_rows('affiliations') ) { ?>
-<?php while (have_rows('affiliations') ): the_row(); 
-$rows = get_field('affiliations');
-$row_count = count($rows);
-$affiliation_name = get_sub_field('aff_name');									   
-?>
-	<?php if ($row_count>=1 && !empty($affiliation_name)): ?>
-		<h5>Affiliations YYY</h5>
-	<?php endif; ?>
-<?php endwhile; ?>	
-	
-		<ul>
-		<?php while (have_rows('affiliations') ): the_row(); ?> 
-			<?php $affiliation_name = get_sub_field('aff_name');
-			if( !empty($affiliation_name) ): ?>
-
-				<li><?php if(get_sub_field('aff_url')) { ?><a href="<?php the_sub_field('aff_url'); ?>" title="<?php the_sub_field('aff_name'); ?>" target="_blank"><?php the_sub_field('aff_name'); ?></a><?php }	else {the_sub_field('aff_name');}?></li>
-
-			<?php endif; ?>
-
-		 <?php endwhile; ?>	
-		 </ul>
-<?php }?>
-						
-						
-						
-						
+								<?php if (have_rows('affiliations') ) { ?>
+									<h5>Affiliations</h5>
+										<ul>
+										<?php while (have_rows('affiliations') ): the_row(); ?> 
+												<li><?php if(get_sub_field('aff_url')) { ?><a href="<?php the_sub_field('aff_url'); ?>" title="<?php the_sub_field('aff_name'); ?>" target="_blank"><?php the_sub_field('aff_name'); ?></a><?php }	else {the_sub_field('aff_name');}?></li>
+										 <?php endwhile; ?>	
+										 </ul>
+								<?php }?>
 							</div>
 						</div>
 					</div></div>
