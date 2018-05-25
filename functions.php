37f477615bc908c6bb169c5c726a797ab3b1b247
cohpa <djanosik@ucf.edu>
2018-05-25 11:21:09 -0400

    tweak

--- functions.php
+++ functions.php
@@ -28,32 +28,12 @@ function themeslug_enqueue_script() { // This is for any 'Colleges-Theme' javasc
 add_action( 'wp_enqueue_scripts', 'themeslug_enqueue_script' );
 
 /* Change Excerpt length */
-function excerpt($limit) {
-  $excerpt = explode(' ', get_the_excerpt(), $limit);
-  if (count($excerpt)>=$limit) {
-    array_pop($excerpt);
-    $excerpt = implode(" ",$excerpt).'...';
-  } else {
-    $excerpt = implode(" ",$excerpt);
-  }	
-  $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
-  return $excerpt;
-}
- 
-function content($limit) {
-  $content = explode(' ', get_the_content(), $limit);
-  if (count($content)>=$limit) {
-    array_pop($content);
-    $content = implode(" ",$content).'...';
-  } else {
-    $content = implode(" ",$content);
-  }	
-  $content = preg_replace('/[.+]/','', $content);
-  $content = apply_filters('the_content', $content); 
-  $content = str_replace(']]>', ']]&gt;', $content);
-  return $content;
-}
 
+
+
+function short_excerpt($string) {
+echo substr($string, 0, 200); 
+}
 
 // CUSTOM SIDE BAR ADDONS
     // ADDING ONE FOR THE NEWS BLOG
