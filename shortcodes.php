2acd03fb415bf92d7f55b57e5dc2118f58239f45
cohpa <djanosik@ucf.edu>
2018-06-07 15:08:58 -0400

    shadow borders

--- shortcodes.php
+++ shortcodes.php
@@ -454,7 +454,7 @@ $args = array(
           
 <?php $getimageURL = wp_get_attachment_url( get_post_thumbnail_id(), 'large' ); ?>
 <div class="row mb-4 personlist-ht">
-	<div class="col-lg-2 col-md-3 col-sm-4 col-4 p-0">
+	<div class="col-lg-2 col-md-3 col-sm-4 col-4 p-0 vc_box_shadow_border vc_box_border_grey">
 
 		   <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
 		   <?php if ( has_post_thumbnail()) { ?>
