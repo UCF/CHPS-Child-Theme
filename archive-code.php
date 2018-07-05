06f015fb9af871668c98c3a83fd7b6fd2dffa199
cohpa <djanosik@ucf.edu>
2018-07-05 15:20:49 -0400

    add archive code

--- archive-code.php
+++ archive-code.php
@@ -8,7 +8,7 @@
 			</header>
 		</div>
 		<div class="col-lg-6">	
-			<?php echo do_shortcode( ' [searchme posttype="person" size="large" placeholder="Search by Name"] ' ); ?>
+			<?php echo do_shortcode( ' [searchme posttype="code" size="large" placeholder="Search by Keyword"] ' ); ?>
 		</div>
 		<div class="col-lg-12 pt-2">
 			<i class="fa fa-search icongrey"></i><span class="searchresults">Search Result for: <strong><?php echo "$s"; ?></strong></span>
@@ -17,10 +17,36 @@
 		<div class="row">
 			<div class="col-lg-9 col-md-12">
 				<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
-				$people_posts = new WP_Query($query_string."&meta_key=profile_L_name&orderby=meta_value&order=ASC");
-				while($people_posts->have_posts()) : $people_posts->the_post();
+				while ( have_posts() ) : the_post();
+				$getimgURL = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' );
+				$display_name = get_the_author_meta( 'display_name', $post->post_author );
+				$categories = get_the_category();
 				?>
-				<?php get_template_part( 'person-result'); ?>
+				<div class="row mb-4 cat-border">
+					<div class="col-lg-3 p-0 media-background-container catlist-photo mx-auto">
+						   <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+						   <?php if ( has_post_thumbnail()) { ?>
+							<img src="<?php echo $getimgURL; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" class="media-background object-fit-cover">
+							<?php } else { ?>
+							<?php switch_to_blog(2);?>	
+								<img src="<?php the_field('default_news_image', 'option'); ?>" alt="<?php the_title_attribute(); ?>" title="<?php the_title_attribute(); ?>" class="media-background object-fit-cover">
+							<?php restore_current_blog(); ?>
+						<?php } ?>
+						   </a>
+					</div>
+					<div class="col-lg-9 p-4"> 
+						<?php 
+							if ( ! empty( $categories ) ) {
+								echo '<a class="category-title" href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+							}
+						?>
+						<h2 class="h5 pt-2 mainnews"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						<span class="authortext">Written By: <?php if(get_field('overwrite_author')){ the_field('overwrite_author');} else { echo $display_name; }?> | <?php the_time('F j, Y'); ?></span>
+						<div class="entry">
+							<?php the_excerpt(); ?>
+						</div>
+					</div>
+				</div>
 				<?php endwhile; ?>
 				<!-- then the pagination links -->
 				<div class="mt-5">
