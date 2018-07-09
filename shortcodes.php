9ce85cafdab66e6cdf6f358a7004f71c5beb9494
cohpa <djanosik@ucf.edu>
2018-07-09 09:06:41 -0400

    removed CSS from shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -75,7 +75,7 @@ function newsvisualvar( $atts ) {
         'category' => '0',
 		'column' => '4',
     ), $atts ); 
-	switch_to_blog(2);
+switch_to_blog(2);
 $category_id = get_cat_ID($a['category']);	
 		$visualnews = new WP_Query(array(
 				'post_type'	=> 'post',
@@ -119,51 +119,6 @@ $category_id = get_cat_ID($a['category']);
 </div>
 <?php wp_reset_query(); ?> 
 <?php restore_current_blog(); ?>
-<style>	
-.visnews-photo {
-    height: 200px;
-}
-@media screen and (max-width: 992px) {
-	.visnews-photo {
-		width: 100%;
-		height:225px;
-	}
-}	
-.newsmedia .visnews {
-  height: 100%;
-  padding: 0px; 
-  color: #000;
-  border: solid 1px #ddd;
-  line-height:17px;
-  font-size: 15px !important;
-  font-weight:500;
-}
-.newsmedia .visnews:hover {
-  background: #eee;
-}	
-.newsmedia .col-xs-12 a {
-    color:#000;
-    text-decoration:none !important;
-}
-.row-flex {
-  display: flex;
-  flex-wrap: wrap;
-}
-[class*="col-"] {
-  margin-bottom: 12px;
-}
-.newsmedia .narrow-gutter [class*='col-'] {
-  padding-right:6px;
-  padding-left:6px;
-}
-.newsmedia .newsdate {
-    margin-top:15px;
-    display:block;
-    font-weight:normal;
-    color: #aaa;
-    font-style: italic;
-} /* START RECURRING STYLING */	
-</style>
 <?php }
 add_shortcode( 'newsvisual', 'newsvisualvar' );
 //  ------------------------------------------------------------------------
