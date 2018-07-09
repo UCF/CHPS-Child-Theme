e19de085bdff6a95d12e77a9c4ee4310de3dde52
cohpa <djanosik@ucf.edu>
2018-07-09 10:40:33 -0400

    reworked shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -65,10 +65,9 @@ function externalnewsvar( $atts ) {
 <?php }
 add_shortcode( 'exnews', 'externalnewsvar' );
 ?><?php
-//  ------------------------------------------------------------------------
+//  ---------------------------------------
 // SHORTCODE TO DISPLAY RECENT NEWS VERTICALLY IN BOXES 
-//
-// [newsvisual number="" category=""]
+// [newsvisual column="" number="" category=""]
 function newsvisualvar( $atts ) {
     $a = shortcode_atts( array(
         'number' => '4',
@@ -85,43 +84,29 @@ $category_id = get_cat_ID($a['category']);
 				'posts_per_page' => $a['number'],
 				'cat' => $category_id,
 				)
-			); ?>							
- <div class="container newsmedia">
-    <div class="row narrow-gutter row-flex">
-		<?php while($visualnews->have_posts()) : $visualnews->the_post();
-	$getimgURL = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large', false )[0];	?><!-- START THE REPEAT SECTION -->   
-			<?php if ($a['column'] == '3') { ?> 
-				<div class="col-lg-4 col-sm-6 col-xs-12">
-					<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
-						<div class="visnews">
-							<div class="media-background-container visnews-photo mx-auto">
-			<?php } else { ?> 
-				<div class="col-lg-3 col-sm-6 col-xs-12">
-					<a href="<?php the_permalink() ?>" title="<?php the_title(); ?>">
-						<div class="visnews">
-							<div class="media-background-container visnews-photo mx-auto">
-			<?php } ?>
-								<?php if ( has_post_thumbnail()) { ?>
-									<img src="<?php echo $getimgURL; ?>" alt="<?php the_title(); ?>" title="<?php the_title(); ?>" class="media-background object-fit-cover">
-									<?php } else { ?>
-									<img src="<?php the_field('default_news_image', 'option'); ?>" alt="<?php the_title_attribute(); ?>" title="<?php the_title_attribute(); ?>" class="media-background object-fit-cover">
-								<?php } ?>
-							</div>
-							<div class="p-3">
-								<?php the_title(); ?>
-								<p class="newsdate"><?php the_time('F j, Y'); ?></p>
-							</div>
-						</div>
-					</a>
-				</div><!-- END OF THE REPEAT SECTION -->		
-		<?php endwhile; ?>
-    </div>
-</div>
-<?php wp_reset_query(); ?> 
-<?php restore_current_blog(); ?>
-<?php }
+			); 
+	$listnews = '<div class="container newsmedia"><div class="row narrow-gutter row-flex">';
+while($visualnews->have_posts()) : $visualnews->the_post();
+$getimgURL = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large', false )[0];
+if ($a['column'] == '3') {	
+	$listnews .= '<div class="col-lg-4 col-sm-6 col-xs-12"><a href="' . the_permalink() . '" title="' . the_title() . '"><div class="visnews"><div class="media-background-container visnews-photo mx-auto">';
+} else {
+	$listnews .= '<div class="col-lg-3 col-sm-6 col-xs-12"><a href="' . the_permalink() . '" title="' . the_title() . '"><div class="visnews"><div class="media-background-container visnews-photo mx-auto">';
+}
+if ( has_post_thumbnail()) {
+	$listnews .= '<img src="' . $getimgURL .'" alt="' . the_title() . '" title="' . the_title() .'" class="media-background object-fit-cover">';
+} else {
+	$listnews .= '<img src="' . the_field('default_news_image', 'option') . '" alt="' . the_title_attribute() . '" title="' . the_title_attribute() . '" class="media-background object-fit-cover">';
+}
+	$listnews .= '</div><div class="p-3">' . the_title() . '<p class="newsdate">' . the_time('F j, Y') . '</p></div></div></a></div>';
+endwhile;
+	$listnews .= '</div></div>';
+return $list;
+wp_reset_query();	
+restore_current_blog();	
+}
 add_shortcode( 'newsvisual', 'newsvisualvar' );
-//  ------------------------------------------------------------------------
+//  ---------------------------------------
 ?><?php
 //  ------------------------------------------------------------------------
 // SHORTCODE TO DISPLAY RECENT NEWS TEXT LINKS
