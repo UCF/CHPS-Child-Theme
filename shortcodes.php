6e82dfb0a3a6d59cf53dbf112a7f1a0f06b6a684
cohpa <djanosik@ucf.edu>
2018-07-10 14:11:37 -0400

    test

--- shortcodes.php
+++ shortcodes.php
@@ -429,7 +429,7 @@ $buildingMap = get_field('building', $post->ID);
 ?>
 <div class="row mb-4 cat-border personlist-ht">
 	<!--<div class="col-lg-2 col-md-3 col-sm-4 col-4 p-0 media-background-container catlist-photo mx-auto">-->
-	<div class="col-4 p-0 media-background-container catlist-photo mx-auto">
+	<div class="col-4 p-0 ">
 		   <a href="<?php echo get_permalink($post->ID); ?>" title="<?php echo $post->post_title; ?>" >
 		   <?php if ( has_post_thumbnail()) { ?>
 				<img src="<?php echo $getimageURL; ?>" alt="<?php echo $post->post_title; ?>'s profile picture at UCF" title="<?php echo $post->post_title; ?>" class="media-background object-fit-cover">
