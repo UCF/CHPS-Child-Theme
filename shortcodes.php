d5b349b97c303ffde65a6f39b383a5a89a734da6
cohpa <djanosik@ucf.edu>
2018-07-10 15:32:02 -0400

    sm

--- shortcodes.php
+++ shortcodes.php
@@ -435,7 +435,7 @@ if ($peeps == '3') {$ellebell = 'col-5';}
 ?>
 <div class="row mb-4 cat-border personlist-ht">
 	<!--<div class="col-lg-2 col-md-3 col-sm-4 col-4 p-0 media-background-container catlist-photo mx-auto">-->
-	<div class="<?php echo $ellebell; ?> col-md-12 p-0 media-background-container catlist-photo mx-auto">
+	<div class="<?php echo $ellebell; ?> col-sm-12 p-0 media-background-container catlist-photo mx-auto">
 		   <a href="<?php echo get_permalink($post->ID); ?>" title="<?php echo $post->post_title; ?>" >
 		   <?php if ( has_post_thumbnail()) { ?>
 				<img src="<?php echo $getimageURL; ?>" alt="<?php echo $post->post_title; ?>'s profile picture at UCF" title="<?php echo $post->post_title; ?>" class="media-background object-fit-cover">
@@ -447,7 +447,7 @@ if ($peeps == '3') {$ellebell = 'col-5';}
 		   </a>  
 	</div>
 	<!-- <div class="col-lg-10 col-md-9 col-sm-8 col-8 p-3"> -->
-	<div class="col p-3">
+	<div class="col col-sm-12 p-3">
 		<h2 class="h4"><a href="<?php echo get_permalink($post->ID); ?>" rel="bookmark" title="<?php echo $post->post_title; ?>"><?php echo $post->post_title; ?></a></h2>
 		<?php
 		if(get_field('job_titles_tax', $post->ID)){ ?>
