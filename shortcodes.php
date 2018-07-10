9ce89fe09a4ce72d173fa60b45562fe700c3945d
cohpa <djanosik@ucf.edu>
2018-07-10 15:46:04 -0400

    tweak

--- shortcodes.php
+++ shortcodes.php
@@ -433,7 +433,6 @@ if ($peeps == '1') {$ellebell = 'col-2';}
 if ($peeps == '2') {$ellebell = 'col-3';}
 if ($peeps == '3') {$ellebell = 'col-5';}
 ?>
-<?php echo $getimageURL; ?>
 <div class="row mb-4 cat-border personlist-ht">
 	<!--<div class="col-lg-2 col-md-3 col-sm-4 col-4 p-0 media-background-container catlist-photo mx-auto">-->
 	<div class="<?php echo $ellebell; ?> p-0 media-background-container catlist-photo mx-auto">
