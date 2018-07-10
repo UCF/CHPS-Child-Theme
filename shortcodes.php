29f23cd8b79a2f8ac4a5f1e11763cd19fd5e4966
cohpa <djanosik@ucf.edu>
2018-07-10 11:21:49 -0400

    tweak

--- shortcodes.php
+++ shortcodes.php
@@ -431,9 +431,9 @@ function showpersonvar( $atts ) {
 $mypost = get_page_by_title( 'John Doe', OBJECT, 'person' );
 	echo $mypost->post_title; 
 	echo 'regular';
-	the_field('email_address', $mypost);
+	the_field('email_address', $mypost->ID);
 	echo 'get the field';
-	get_the_field('email_address', $mypost);
+	get_the_field('email_address', $mypost->ID);
 ?>
 <?php } add_shortcode( 'showperson', 'showpersonvar' );
 //  ------------------------------------------------------------------------
