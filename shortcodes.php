a75e373f363ea95ad441c081cdc8be8828663269
cohpa <djanosik@ucf.edu>
2018-07-10 11:23:14 -0400

    tweak

--- shortcodes.php
+++ shortcodes.php
@@ -433,7 +433,7 @@ $mypost = get_page_by_title( 'John Doe', OBJECT, 'person' );
 	echo 'regular';
 	the_field('email_address', $mypost->ID);
 	echo 'get the field';
-	get_the_field('email_address', $mypost->ID);
+	//get_the_field('email_address', $mypost->ID);
 ?>
 <?php } add_shortcode( 'showperson', 'showpersonvar' );
 //  ------------------------------------------------------------------------
