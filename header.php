589c4ab10d19a77ccf946aec54dd63cc96f5b127
cohpa <djanosik@ucf.edu>
2018-07-14 14:40:44 -0400

    tweak

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 if ( get_field( 'breadcrumb', 'option' ) == 1 ) { ?>
 <div class="breadcrumbnav">
 	<div class="container">
-		<a href="/" title="UCF College of Health Professions and Sciences" class="yellow">College of Health Professions and Sciences</a><span class="hidemobile"> > <a href="<?php echo get_site_url(); ?>" title="<?php bloginfo( 'name' ); ?> at the College of Health Professions and Sciences"><?php bloginfo( 'name' ); ?></a>
+		<a href="/" title="UCF College of Health Professions and Sciences" class="yellow">College of Health Professions and Sciences</a><span class="hidemobile"> > <a href="<?php echo get_site_url(); ?>" title="<?php bloginfo( 'name' ); ?> at the UCF College of Health Professions and Sciences"><?php bloginfo( 'name' ); ?></a>
        	<?php if ( !is_front_page() ) { ?>
          	> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?> at the <?php bloginfo( 'name' ); ?>"><?php the_title(); ?></a></span>
     	<?php } ?>
