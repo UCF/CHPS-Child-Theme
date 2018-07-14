a7f307325d55114f3c8fbe09dbd9f12d774bc671
cohpa <djanosik@ucf.edu>
2018-07-14 14:37:57 -0400

    moved iF

--- header.php
+++ header.php
@@ -10,11 +10,13 @@
 		</header>
 		<main id="main" class="site-main">
 <?php 
-if ( !is_front_page() ) {
 if ( get_field( 'breadcrumb', 'option' ) == 1 ) { ?>
 <div class="breadcrumbnav">
 	<div class="container">
-		<a href="/" title="UCF College of Health Professions and Sciences" class="yellow">College of Health Professions and Sciences</a><span class="hidemobile"> > <a href="<?php echo get_site_url(); ?>" title="<?php bloginfo( 'name' ); ?> at the College of Health Professions and Sciences"><?php bloginfo( 'name' ); ?></a> > <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?> at the <?php bloginfo( 'name' ); ?>"><?php the_title(); ?></a></span>
+		<a href="/" title="UCF College of Health Professions and Sciences" class="yellow">College of Health Professions and Sciences</a><span class="hidemobile"> > <a href="<?php echo get_site_url(); ?>" title="<?php bloginfo( 'name' ); ?> at the College of Health Professions and Sciences"><?php bloginfo( 'name' ); ?></a>
+       	<?php if ( !is_front_page() ) { ?>
+         	> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?> at the <?php bloginfo( 'name' ); ?>"><?php the_title(); ?></a></span>
+    	<?php } ?>
     </div>
 </div>
-<?php } }?>
+<?php } ?>
