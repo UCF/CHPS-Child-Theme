1dd4d24478a40b2e281e5aee40b48490481b6d7a
cohpa <djanosik@ucf.edu>
2018-08-27 14:02:41 -0400

    tweak

--- single-person.php
+++ single-person.php
@@ -126,9 +126,7 @@ $ids = get_the_ID();
 			E-mail 333
 		</div>
 		<div class="col-xl-8 col-md-12 col-sm-8 person-attr">
-			<?php
-			  if( in_array( "Yes", get_field( 'hide_email' ) ) ) {
-			?>
+			<?php if( get_field( 'hide_email' )) {  ?>
 			<a href="mailto:<?php the_field('alternate_email'); ?>">
 				<?php the_field('alternate_email'); ?>
 			</a>			
