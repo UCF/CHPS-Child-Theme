f844800c2aea4e3de67be0c339df71b80bd0de74
cohpa <djanosik@ucf.edu>
2019-10-24 09:48:24 -0400

    2 secs

--- single.php
+++ single.php
@@ -10,7 +10,7 @@ $alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
 ?>
 <?php if ( get_field( 'updatenewstype' ) == 1 ) { 
  // echo 'true';
-	echo '<meta http-equiv="refresh" content="3;url=' . get_field('updatenewsURL') . '" />';
+	echo '<meta http-equiv="refresh" content="2;url=' . get_field('updatenewsURL') . '" />';
 
 	
 } else { 
