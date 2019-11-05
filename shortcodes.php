f800553995c14a5a556a6789b43b7102593bc803
cohpa <djanosik@ucf.edu>
2019-11-05 13:37:35 -0500

    yyy

--- shortcodes.php
+++ shortcodes.php
@@ -45,16 +45,20 @@ $listnews = '<div class="container newsmedia"><div class="row narrow-gutter row-
 while($visualnews->have_posts()) : $visualnews->the_post();
 	$getimgURL = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large', false )[0];
 	if ($a['column'] == '3') {	
-		$listnews .= '<div class="col-lg-4 col-sm-6 col-xs-12"><a href="' . get_the_permalink() . '" title="' . get_the_title() . '"><div class="visnews"><div class="media-background-container visnews-photo mx-auto">';
+		$listnews .= '<div class="col-lg-4 col-sm-6 col-xs-12">';
+		$listnews .= '<a href="' . get_the_permalink() . '" title="' . get_the_title() . '">';
+		$listnews .= '<div class="visnews"><div class="media-background-container visnews-photo mx-auto">';
 	} else {	
-		$listnews .= '<div class="col-lg-3 col-sm-6 col-xs-12"><a href="' . get_the_permalink() . '" title="' . get_the_title() . '"><div class="visnews"><div class="media-background-container visnews-photo mx-auto">';
+		$listnews .= '<div class="col-lg-3 col-sm-6 col-xs-12">';
+		$listnews .= '<a href="' . get_the_permalink() . '" title="' . get_the_title() . '">';
+		$listnews .= '<div class="visnews"><div class="media-background-container visnews-photo mx-auto">';
 	}	
 	if ( has_post_thumbnail()) {	
 		$listnews .= '<img src="' . $getimgURL . '" alt="' . get_the_title() . '" title="' . get_the_title() . '" class="media-background object-fit-cover">';
 	} else { 	
 		$listnews .= '<img src="' . get_field('default_news_image', 'option') . '" alt="' . get_the_title() . '" title="' . get_the_title() . '" class="media-background object-fit-cover">';
 	}	
-	$listnews .= '</div><div class="p-3">xxx' . get_the_title() . '<p class="newsdate">' . get_the_time('F j, Y') . '</p></div></div></a></div>';	
+	$listnews .= '</div><div class="p-3">yyy' . get_the_title() . '<p class="newsdate">' . get_the_time('F j, Y') . '</p></div></div></a></div>';	
 endwhile;
 $listnews .= '</div></div>';	
 wp_reset_query();
