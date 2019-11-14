97d112317878f7a29bd03f44b3fcabf47d6e3e20
cohpa <djanosik@ucf.edu>
2019-11-14 14:09:40 -0500

    add degrees to title

--- person-result.php
+++ person-result.php
@@ -14,7 +14,14 @@ $buildingMap = get_field('building');
 		   </a> 
 	</div>
 	<div class="col-lg-7 col-md-9 col-sm-8 col-8 p-3">
-		<h2 class="h4"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		<h2 class="h4"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?><?php if( get_field('degrees') ) {
+							while ( have_rows('degrees') ) : the_row();
+							 $arrayDegree[] = get_sub_field('degree'); 
+							endwhile;
+							$degreeIDs = implode(', ', $arrayDegree);
+						   echo '<span class"">, ' . $degreeIDs . '</span>';
+						}
+						?></a></h2>
 		<?php
 		if(get_field('job_titles_tax')){ ?>
 			<div class="profilejobtitle">
