4726b329a7ebb9d2cb76b97c5d20895d9504f02e
cohpa <djanosik@ucf.edu>
2019-11-15 12:52:04 -0500

    label

--- single-person.php
+++ single-person.php
@@ -249,7 +249,7 @@ $field = get_sub_field_object('field_5dcdb34dd0159');
 $value = get_sub_field('degree_select');
 $label = $field['choices'][ $value ];
 ?>
-											<li>LABEL: <?php echo $field['value']; ?>--55--<?php the_sub_field('degree_select'); ?><?php if(get_sub_field('degree_discipline')) { ?>, <?php the_sub_field('degree_discipline'); }?><?php if(get_sub_field('degree_location')) { ?>, <?php the_sub_field('degree_location'); }?>
+											<li>LABEL: <?php echo $field['label']; ?>--55--<?php the_sub_field('degree_select'); ?><?php if(get_sub_field('degree_discipline')) { ?>, <?php the_sub_field('degree_discipline'); }?><?php if(get_sub_field('degree_location')) { ?>, <?php the_sub_field('degree_location'); }?>
 													</li>
 
 											 <?php endwhile; ?>	
