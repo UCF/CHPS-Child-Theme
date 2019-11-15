849aaacb09e53e17e054593b0dfca36728d3b1f5
cohpa <djanosik@ucf.edu>
2019-11-15 12:35:50 -0500

    44

--- single-person.php
+++ single-person.php
@@ -249,7 +249,7 @@ $field = get_field_object('field_5dcdb34dd0159');
 $value = get_field('degree_select');
 $label = $field['choices'][ $value ];
 ?>
-											<li>LABEL: <?php echo $label; ?>---33-<?php the_sub_field('degree_select'); ?><?php if(get_sub_field('degree_discipline')) { ?>, <?php the_sub_field('degree_discipline'); }?><?php if(get_sub_field('degree_location')) { ?>, <?php the_sub_field('degree_location'); }?>
+											<li>LABEL: <?php echo $label; ?>---44-<?php the_sub_field('degree_select'); ?><?php if(get_sub_field('degree_discipline')) { ?>, <?php the_sub_field('degree_discipline'); }?><?php if(get_sub_field('degree_location')) { ?>, <?php the_sub_field('degree_location'); }?>
 													</li>
 
 											 <?php endwhile; ?>	
