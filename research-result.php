97af9ece4a8148adf8643b4fa39563f0cce600cb
cohpa <djanosik@ucf.edu>
2019-11-21 10:01:23 -0500

    research list

--- research-result.php
+++ research-result.php
@@ -14,11 +14,11 @@
 	<div class="col-lg-9 col-md-9 col-sm-8 col-8 px-4 py-0">
 		<h2 class="h4"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?><?php if( get_field('degrees') ) {
 							while ( have_rows('degrees') ) : the_row();
-							 $arrayDegree[] = get_sub_field('degree'); 
+							 $array[] = get_sub_field('degree_select'); 
 							endwhile;
-							$degreeIDs = implode(', ', $arrayDegree);
-						   echo '<span class"">, ' . $degreeIDs . '</span>';
-						}
+							$foo = implode(', ', array_column($array, 'label'));
+							echo '<span class"">, ' . $foo . '</span>';
+							}								
 						?></a></h2>
 		<?php
 		if(get_field('job_titles_tax')){ ?>
