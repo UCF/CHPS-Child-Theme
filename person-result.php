549cbaa4edf53b9d9d92b765b715f01a97678411
cohpa <djanosik@ucf.edu>
2019-11-26 14:49:05 -0500

    photo no longer resizes

--- person-result.php
+++ person-result.php
@@ -2,7 +2,7 @@
 $buildingMap = get_field('building');
 ?>
 <div class="row mb-4 cat-border personlist-ht">
-	<div class="col-lg-2 col-md-3 col-sm-4 col-4 p-0 media-background-container catlist-photo mx-auto">
+	<div class="col-lg-2 col-md-3 col-sm-4 col-4 p-0">
 		   <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
 		   <?php if ( has_post_thumbnail()) { ?>
 				<img src="<?php echo $getimageURL; ?>" alt="<?php echo get_person_name( $post ); ?>'s profile picture at UCF" title="<?php $post->post_title; ?>" class="media-background object-fit-cover">
