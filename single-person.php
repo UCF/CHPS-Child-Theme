c342e05ccd6229b480a6b6600c94683dec6ceda2
cohpa <djanosik@ucf.edu>
2019-12-02 16:59:37 -0500

    title txt

--- single-person.php
+++ single-person.php
@@ -277,7 +277,7 @@ $posts = get_posts(array(
 								<?php endif; ?>
 								<?php if(get_field('google_scholar')) { ?>
 								<div class="btnhover-black" style="margin-top: 20px;">
-										<a style="background-color:#000000; color:#ffffff; padding: 18px 25px; font-size: 16px; text-decoration: none;" class="" href="<?php the_field('google_scholar'); ?>" title="" target="_blank"><i class="vc_btn3-icon fa fa-bookmark" style="font-size: 20px; height: 16px; line-height: 16px; margin-right: 15px;"></i> View Bibliography</a>
+										<a style="background-color:#000000; color:#ffffff; padding: 18px 25px; font-size: 16px; text-decoration: none;" class="" href="<?php the_field('google_scholar'); ?>" title="View <?php echo get_person_name( $post ); ?>'s Bibliography" target="_blank"><i class="vc_btn3-icon fa fa-bookmark" style="font-size: 20px; height: 16px; line-height: 16px; margin-right: 15px;"></i> View Bibliography</a>
 								</div>
 								<?php }?>
 							</div>
