67b639f9bf2d763909276808e417674cede730a4
cohpa <djanosik@ucf.edu>
2019-12-13 12:42:13 -0500

    new divs

--- single-researchopp.php
+++ single-researchopp.php
@@ -69,25 +69,27 @@ $thumb_id = get_post_thumbnail_id(get_the_ID());
 		</div>
 	</div>
 	<div class="row">
-			<?php echo do_shortcode('[vc_separator style="shadow" border_width="5"]'); ?>
-			<div>
-				<h2 class="heading-underline">Related Research Participation Opportunities</h2> <!-- style this size -->	
-			</div>
-			<div class="col-md-4" style="padding:10px;">
-				<div style="border:1px #d8d8d8 solid; padding:25px;">
-					<a href="#">This is a title of a related research opportunity connected by Unit which could be a long title still</a>
-				</div>	
-			</div>
-			<div class="col-md-4" style="padding:10px;">
-				<div style="border:1px #d8d8d8 solid; padding:25px;">
-					<a href="#">This is a title of a related research opportunity connected by Unit which could be a long title still</a>
-				</div>	
-			</div>
-			<div class="col-md-4" style="padding:10px;">
-				<div style="border:1px #d8d8d8 solid; padding:25px;">
-					<a href="#">This is a title of a related research opportunity connected by Unit which could be a long title still</a>
-				</div>	
-			</div>
+		<?php echo do_shortcode('[vc_separator style="shadow" border_width="5"]'); ?>
+		<div>
+			<h2 class="heading-underline">Related Research Participation Opportunities</h2> <!-- style this size -->	
+		</div>
+	</div>	
+	<div class="row">	
+		<div class="col-md-4" style="padding:10px;">
+			<div style="border:1px #d8d8d8 solid; padding:25px;">
+				<a href="#">This is a title of a related research opportunity connected by Unit which could be a long title still</a>
+			</div>	
+		</div>
+		<div class="col-md-4" style="padding:10px;">
+			<div style="border:1px #d8d8d8 solid; padding:25px;">
+				<a href="#">This is a title of a related research opportunity connected by Unit which could be a long title still</a>
+			</div>	
+		</div>
+		<div class="col-md-4" style="padding:10px;">
+			<div style="border:1px #d8d8d8 solid; padding:25px;">
+				<a href="#">This is a title of a related research opportunity connected by Unit which could be a long title still</a>
+			</div>	
+		</div>
 	</div>
 	</article>
 </div>
