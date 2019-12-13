06015faa9339f06ff8da3db66e5181e9b39620bb
cohpa <djanosik@ucf.edu>
2019-12-13 12:11:01 -0500

    added icons

--- single-researchopp.php
+++ single-researchopp.php
@@ -35,27 +35,27 @@ $thumb_id = get_post_thumbnail_id(get_the_ID());
 				INSERT BUTTON HERE  <!-- hopefully with GLIDE ALSO MAKE THIS GO AWAY ON MOBILE-->
 			</div>
 			<div class="researchOpp-dblock"> 
-				<strong>Recruitment Deadline:</strong><br>
+				<i class="fa fa-map-marker icongrey"></i> <strong>Recruitment Deadline:</strong><br>
 				<span style="font-size: 25px;"><?php the_field('research_enddate'); ?></span>  <!-- responsive size? -->
 			</div>
 			<div class="researchOpp-dblock"> 
-				<strong>Location:</strong><br>
+				<i class="fa fa-map-marker icongrey"></i> <strong>Location:</strong><br>
 				<?php the_field('research_building'); ?><br>   <!-- add link to map maybe? -->
 				<?php the_field('research_address'); ?><br>
 				<?php the_field('research_city'); ?>, <?php the_field('research_state'); ?> <?php the_field('research_zip'); ?>
 			</div>
 			<div class="researchOpp-dblock"> 
-				<strong>Time Commitment:</strong><br>
+				<i class="fa fa-map-marker icongrey"></i> <strong>Time Commitment:</strong><br>
 				Number of visits: <?php the_field('research_visits'); ?><br>
 				Expected time per visit: <?php the_field('research_time'); ?> <?php the_field('research_timemeasure'); ?>
 			</div>
 			<div class="researchOpp-dblock"> 
-				<strong>Compensation:</strong><br>
+				<i class="fa fa-map-marker icongrey"></i> <strong>Compensation:</strong><br>
 				Amount: $<?php the_field('research_amount'); ?>.00 (USD)<br>
 				Format: <?php the_field('research_payment_format'); ?>
 			</div>
 			<div class="researchOpp-dblock"> 
-				<strong>Contact:</strong><br>
+				<i class="fa fa-map-marker icongrey"></i> <strong>Contact:</strong><br>
 				<?php the_field('research_time'); ?>
 			</div>
 			<div class="researchOpp-dblock"> 
