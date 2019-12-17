a81be5f7d56a9055928f3ccfa3bd6d52b9b55cc5
cohpa <djanosik@ucf.edu>
2019-12-17 10:46:25 -0500

    video header

--- archive-researchopp.php
+++ archive-researchopp.php
@@ -1,19 +1,32 @@
 <?php get_header();?>
 <div class="container mb-5 mt-3 mt-lg-5">
 	<article class="<?php echo $post->post_status; ?> post-list-item">
-	<div class="row mb-4">
-		<div class="col-lg-6">	
-			<header class="archive-header">
-				<h1 class="archive-title heading-underline">Research Opportunities<h1>
-			</header>
+		<div class="header-media  header-media-default media-background-container mb-0 d-flex flex-column">
+			<video class="hidden-xs-down media-background media-background-video object-fit-cover" autoplay muted playsinline loop>
+				<source src="https://healthprofessions.ucf.edu/wp-content/uploads/sites/2/2019/10/UndergradResearch-Header.webm" type="video/webm">
+				<source src="https://healthprofessions.ucf.edu/wp-content/uploads/sites/2/2019/10/UndergradResearch-Header.mp4" type="video/mp4">
+			</video>
+			<button class="media-background-video-toggle btn play-enabled hidden-xs-up" type="button" data-toggle="button" aria-pressed="false" aria-label="Play or pause background videos">
+				<span class="fa fa-pause media-background-video-pause" aria-hidden="true"></span>
+				<span class="fa fa-play media-background-video-play" aria-hidden="true"></span>
+			</button>
+			<picture class="media-background-picture ">
+				<source srcset="https://i1.wp.com/healthprofessions.ucf.edu/wp-content/uploads/sites/2/2019/09/COHPA-building-BG.jpg?resize=575%2C575&ssl=1" media="(max-width: 575px)">
+				<img class="media-background object-fit-cover" src="https://i1.wp.com/healthprofessions.ucf.edu/wp-content/uploads/sites/2/2019/09/COHPA-building-BG.jpg?resize=1600%2C525&ssl=1" alt="">
+			</picture>
+			<div class="header-content">
+				<div class="container d-flex align-items-center align-items-sm-end">
+					<div class="row no-gutters w-100">
+						<div class="col-xl-10">
+							<div class="header-title-wrapper">
+								<h1 class="header-title">Research Participation</h1>
+								<p class="header-subtitle">College of Health Professions and Sciences</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-		<div class="col-lg-6">	
-			<?php echo do_shortcode( ' [searchme posttype="code" size="large" placeholder="Search by Keyword"] ' ); ?>
-		</div>
-		<div class="col-lg-12 pt-2">
-			<i class="fa fa-search icongrey"></i><span class="searchresults">Search Result for: <strong><?php echo "$s"; ?></strong></span>
-		</div>
-	</div>
 		<div class="row">
 			<div class="col-lg-8 col-md-12">
 				<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
