e3a0dbf1d941f5cb2671cbfa8074fa7a6a9a9d3e
cohpa <djanosik@ucf.edu>
2019-12-18 14:11:06 -0500

    blank

--- shortcodes.php
+++ shortcodes.php
@@ -101,10 +101,10 @@ add_shortcode( 'newsvisual', 'newsvisualvar' );
 //
 // [recentexnews category="" number=""]
 function recexnewsvar( $atts ) {
-	$cattitle = single_cat_title();
+	//$cattitle = single_cat_title();
     $b = shortcode_atts( array(
         'number' => '10',
-		'category' => $cattitle,
+		//'category' => $cattitle,
     ), $atts );
 		$recexnews = new WP_Query(array(
 				'post_type'	=> 'inthemedia',
@@ -115,7 +115,7 @@ function recexnewsvar( $atts ) {
 					array (
 						'taxonomy' => 'externalnews_unit',
 						'field' => 'name',
-						'terms' => $b['category'],
+						'terms' => '',
 					)
 				),
 				'posts_per_page' => $b['number'],
