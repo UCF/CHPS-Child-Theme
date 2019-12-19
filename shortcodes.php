0587eb411cd7b88511c04e47ee9a93389b78102f
cohpa <djanosik@ucf.edu>
2019-12-19 10:20:44 -0500

    if empty

--- shortcodes.php
+++ shortcodes.php
@@ -112,9 +112,10 @@ while($visualnews->have_posts()) : $visualnews->the_post();
 		$listnews .= '<img src="' . get_field('default_news_image', 'option') . '" alt="' . get_the_title() . '" title="' . get_the_title() . '" class="media-background object-fit-cover">';
 	}	
 	$listnews .= '</div><div class="p-3">';
-	if ( $a['showcats']='Yes') {
-	$listnews .= '<div class="mb-2"><a class="category-title" href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . $categories[0]->name . '</a></div>';
+	if ( !empty($a['showcats'])) {
+		$listnews .= '<div class="mb-2"><a class="category-title" href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . $categories[0]->name . '</a></div>';
 	}
+	else { }
 	$listnews .= '' . get_the_title() . '</div></div></a></div>';
 endwhile;
 $listnews .= '</div></div>';	
