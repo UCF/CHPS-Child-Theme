a0b32eccf0772e0580f03429eee0051b921d5faf
cohpa <djanosik@ucf.edu>
2020-03-02 22:31:46 -0500

    New tags

--- single.php
+++ single.php
@@ -67,6 +67,20 @@ $alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
 					   <?php endif; ?>
                    <?php } ?>
                     <?php the_content(); ?> 
+                    
+                   	<div class="chps-tag-cloud mb-4 mb-md-5 mt-4 mt-md-5">
+						<h2 class="h6 text-uppercase text-default-aw mb-4">More Topics</h2>
+                        <?php $categories = get_the_category();
+                        $separator = ' ';
+                        $output = '';
+                        if ( ! empty( $categories ) ) {
+                            foreach( $categories as $category ) {
+                                $output .= '<a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
+                            }
+                            echo trim( $output, $separator );
+                        } ?> 
+					</div> 
+                    
                 <?php
                 	endwhile; //resetting the page loop
                 	wp_reset_query(); //resetting the page query
