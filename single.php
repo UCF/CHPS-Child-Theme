abb0d936e16926f2d2172e156f035e1c95f6a9c3
cohpa <djanosik@ucf.edu>
2020-03-03 13:52:09 -0500

    External link

--- single.php
+++ single.php
@@ -119,8 +119,12 @@ $wpex_query = new wp_query( $args );
 foreach( $wpex_query->posts as $post ) : setup_postdata( $post ); 
 $getrelatedIMG = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large', false )[0];
 ?>
-  	<div class="col-lg-4 col-sm-6 col-xs-12">
-		<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+  	<div class="col-lg-4 col-sm-6 col-xs-12">        
+        <?php if ( the_field( 'updatenewstype' ) == 1 ) { ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>" target="_blank">
+		<?php } else { ?>
+			<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+		<?php } ?>
         	<div class="visnews">
             	<div class="media-background-container visnews-photo mx-auto">
                 	<?php if ( has_post_thumbnail()) { ?>
