f348c38666bf6910cb0dca2a2f65d9de2ab86cd5
cohpa <djanosik@ucf.edu>
2020-03-21 13:36:02 -0400

    clean

--- archive-grants.php
+++ archive-grants.php
@@ -60,11 +60,10 @@
 							}?>
 							<i class="fa fa-clock-o iconyellow" style="margin-left:25px;"></i> Deadline: <?php echo $deadlineShort; ?>
 							<i class="fa fa-usd iconyellow" style="margin-left:25px;"></i> <?php if ($payment == 'Yes') { echo 'Paid'; } else { echo 'None'; } ?>
-						<h3 class="h6 mt-2 ml-3">DETAILS:</h3> <!-- style this size -->
-						<div class="mb-2 researchOpp-quals">
+						<div class="mb-1 researchOpp-quals">
 							$<?php the_field('money'); ?> | <?php the_field('start_date'); ?> - <?php the_field('end_date'); ?>
 						</div>
-                        <div class="mb-2">
+                        <div class="mb-1">
 							<?php the_content(); ?>
 						</div>
 					</div>
