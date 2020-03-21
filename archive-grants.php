c8291d3643865259ba98daa3c2903462b4ef26eb
cohpa <djanosik@ucf.edu>
2020-03-21 17:17:28 -0400

    grant_facultymember

--- archive-grants.php
+++ archive-grants.php
@@ -41,8 +41,11 @@
 				<div class="row mb-1">
 					<div class="col-lg-12 p-3" style="border-bottom: 1px solid #EBEBEB"> 
 						<h2 class="h5 mt-2"><?php the_title(); ?></h2>
-			<?php while(has_sub_field('grant_people')): ?>
-                <li><?php the_sub_field('title'); ?>: <?php the_sub_field('regular_person'); ?></li>
+			<?php 
+			while(has_sub_field('grant_people')): 
+			$grant_facultymember = get_sub_field( 'grant_faculty' ); 
+			?>
+                <li><?php the_sub_field('title'); ?>: <?php echo $grant_facultymember; ?><?php the_sub_field('regular_person'); ?></li>
             <?php endwhile; ?>
 						<div>
 							$<?php the_field('money'); ?>, <?php the_field('start_date'); ?> - <?php the_field('end_date'); ?>
