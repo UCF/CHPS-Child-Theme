076d6d761c951435d570d4a69216b9d804b6877a
cohpa <djanosik@ucf.edu>
2020-03-22 11:01:50 -0400

    xx

--- single-person.php
+++ single-person.php
@@ -280,13 +280,16 @@ header .container .h1, header .container .lead {
 									</ul>
 								<?php endif; ?>
                                 <div class="mb-4 pt-3" style="border-top: 1px #ddd solid; ">
-									new = =
+									new XX
 									<?php 
 						// filter
 						function my_posts_where( $where ) {
-							
-							$where = str_replace("meta_key = 'grant_people_$", "meta_key LIKE 'grant_people_%", $where);
-						
+							global $wpdb;
+							$where = str_replace(
+									  "meta_key = 'grant_people_%", 
+									  "meta_key LIKE 'grant_people_%",
+									  $wpdb->remove_placeholder_escape($where)
+							);
 							return $where;
 						}
 						
@@ -301,7 +304,7 @@ header .container .h1, header .container .lead {
 							'post_type' => 'grants',
 							'meta_query' => array(
 								array(
-									'key' => 'grant_people_$_grant_faculty', // name of custom sub field
+									'key' => 'grant_people_%_grant_faculty', // name of custom sub field
 									'value' => $ids, // keep this to match current profile
 									'compare' => '='
 								)
