1ba0d96688406d08ee75aa94bdaa292a0c1a9825
cohpa <djanosik@ucf.edu>
2020-03-22 18:41:08 -0400

    Pad

--- single-person.php
+++ single-person.php
@@ -324,7 +324,7 @@ add_filter('posts_where', 'my_posts_where');
 		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); 
 			$money_output = get_field('grant_money'); 
 		?>
-                <div class="nobullets" style="font-size:14px !important; border-bottom:1px solid #EBEBEB;">
+                <div class="nobullets pl-3" style="font-size:14px !important; border-bottom:1px solid #EBEBEB;">
                 	<h6 class="mb-1 mt-3"><?php the_title(); ?></h6>
                     <div>
                         <span class="grant-money" style="font-size:16px !important;">$<?php echo number_format($money_output, 0, '.', ','); ?></span>
