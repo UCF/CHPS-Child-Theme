7a9f09c0067ca7a3903b9d0111c8cdcf73dbe50b
cohpa <djanosik@ucf.edu>
2020-03-28 17:22:34 -0400

    Temp

--- archive-grants.php
+++ archive-grants.php
@@ -33,43 +33,9 @@
 				<?php $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 				while ( have_posts() ) : the_post();
 				?>
-				<div class="row mb-1 nobullets">
-					<div class="col-lg-12 p-3" style="border-bottom: 1px solid #EBEBEB;"> 
-						<?php 
-						$terms = get_the_terms( $post->ID , 'grant_units' );
-							foreach ( $terms as $term ) {
-							echo '<span class="grant-unit">' . $term->name . '</span>';
-						} ?>
-                        <h2 class="h5 mt-1"><?php the_title(); ?></h2>
-                            <div class="row">
-                              <div class="col-12">
-                                <?php 
-                                while(has_sub_field('grant_people')):
-                                $grant_facultymember = get_sub_field( 'grant_faculty' ); 
-								$money_output = get_field('grant_money');
-                                ?>
-                                    <li><strong><?php the_sub_field('title'); ?>:</strong> <?php the_sub_field('regular_person'); ?>
-                                    <?php foreach( $grant_facultymember as $post_object): ?>
-                                        <a href="<?php echo get_permalink($post_object->ID); ?>"><?php echo get_the_title($post_object->ID); ?></a>
-                                    <?php endforeach; ?>
-                                    </li>
-                                <?php endwhile; ?>
-                                <strong>Amount:</strong> $<?php echo number_format($money_output, 0, '.', ','); ?><?php if( get_field('grant_start_date')) {  ?> | <strong>Timeframe:</strong> <?php the_field('grant_start_date'); ?> <?php if( get_field('grant_end_date')) {  ?>- <?php the_field('grant_end_date'); ?><?php } ?>
-                                <?php } ?>
-                              </div>
-                            </div>
-                        <div>
-							<strong>Category:</strong> <?php 
-						$terms = get_the_terms( $post->ID , 'grant_cats' );
-							foreach ( $terms as $term ) {
-							echo $term->name;
-						} ?>
-						</div>    
-                        <div>
-							<strong>Funding Agency:</strong> <?php the_field('grant_agency'); ?>
-						</div>
-					</div>
-				</div>
+				<div class="pt-3 pb-3 grantResult">
+					<?php get_template_part( 'grant-results'); ?>
+                </div>
 				<?php endwhile; ?>
 				<!-- then the pagination links -->
 				<div class="mt-5">
