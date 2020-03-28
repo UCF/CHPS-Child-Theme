bf95fefada7056ee79f4024eddccd941686b297b
cohpa <djanosik@ucf.edu>
2020-03-28 05:13:23 -0400

    Revert

--- shortcodes.php
+++ shortcodes.php
@@ -631,12 +631,36 @@ else {
 $loop = new WP_Query($args);
 while($loop->have_posts()) : $loop->the_post(); 
 $money_output = get_field('grant_money'); 
-$listgrants = '<div class="nobullets pl-3 pb-3" style="font-size:14px !important; border-bottom:1px solid #EBEBEB;">';
-$listgrants .= '<h6 class="mb-1 mt-3">' . get_the_title() . '</h6><div>';
-endwhile;
-wp_reset_query();
-return $listgrants;
 ?>
+<div class="nobullets pl-3 pb-3" style="font-size:14px !important; border-bottom:1px solid #EBEBEB;">
+    <h6 class="mb-1 mt-3"><?php the_title(); ?></h6>
+    <div>
+        <?php 
+        while(has_sub_field('grant_people')):
+        $grant_facultymember = get_sub_field( 'grant_faculty' ); 
+        ?>
+            <li><strong><?php the_sub_field('title'); ?>:</strong> <?php the_sub_field('regular_person'); ?>
+            <?php foreach( $grant_facultymember as $post_object): ?>
+                <a href="<?php echo get_permalink($post_object->ID); ?>"><?php echo get_the_title($post_object->ID); ?></a>
+            <?php endforeach; ?>
+            </li>
+        <?php endwhile; ?>
+        <strong>Amount:</strong> $<?php echo number_format($money_output, 0, '.', ','); ?><?php if( get_field('grant_start_date')) {  ?>    
+            | <strong>Timeframe:</strong> <?php the_field('grant_start_date'); ?> <?php if( get_field('grant_end_date')) {  ?>- <?php the_field('grant_end_date'); ?><?php } ?>
+        <?php } ?>
+        <div>
+            <strong>Categories:</strong> <?php 
+        $terms = get_the_terms( $post->ID , 'grant_cats' );
+            foreach ( $terms as $term ) {
+            echo $term->name;
+        } ?>
+        </div>
+        <div>
+            <strong>Funding Agency:</strong> <?php the_field('grant_agency'); ?>
+        </div>    	
+    </div>
+</div>
+<?php endwhile; ?>
 <div class="mt-5">
 	<?php wpbeginner_numeric_posts_nav(); ?>
 </div>
