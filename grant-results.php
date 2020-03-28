f82c033e260b59d14ee46913b6f51112d0997276
cohpa <djanosik@ucf.edu>
2020-03-28 14:59:40 -0400

    mt

--- grant-results.php
+++ grant-results.php
@@ -4,7 +4,7 @@ $terms = get_the_terms( $post->ID , 'grant_units' );
 	echo '<span class="grant-unit">' . $term->name . '</span>';
 } ?>
 <div class="nobullets">
-    <h3 class="mb-1 mt-3"><?php the_title(); ?></h3>
+    <h3 class="mb-1"><?php the_title(); ?></h3>
     <div>
         <?php 
 		$money_output = get_field('grant_money');
