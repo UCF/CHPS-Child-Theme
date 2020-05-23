a8a2401e5ec01af5e13608c070d80a811bb14b77
cohpa <djanosik@ucf.edu>
2020-05-23 12:32:32 -0400

    Clean up

--- footer.php
+++ footer.php
@@ -98,19 +98,20 @@
 }
 </style>		
 <?php the_field('bonus_scripts', 'option'); ?>	
-
+<?php if ( get_field( 'enablepopdom', 'option' ) == 1 ):  ?>
+	<script src='https://cdn1.pdmntn.com/a/Byg7AfczV.js'></script>
+<?php endif; ?>	
 <?php if ( get_field( 'enablehotjar', 'option' ) == 1 ):  ?>
-<!-- Hotjar Tracking Code for https://healthprofessions.ucf.edu -->
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:934323,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:934323,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 <?php endif; ?>
 <!-- END OF CODE - DAVID JANOSIK REPLICATING UCF FOOTER --> 	
 <?php restore_current_blog(); ?>		
