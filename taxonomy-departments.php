f72e23dd24c2731a76cfdafc629916cc1937762c
cohpa <djanosik@ucf.edu>
2020-08-31 10:49:01 -0400

    Add assistants

--- taxonomy-departments.php
+++ taxonomy-departments.php
@@ -80,6 +80,56 @@ $tax = $wp_query->get_queried_object();
 <!-- END PART TIME FACULTY -->
 					<?php //wpbeginner_numeric_posts_nav(); ?>
 				</div>
+                <div class="mt-5 pt-3">
+				<!-- START ASSISTANTS -->
+					<?php
+					$argsAssist = array(
+					  'post_type'   => 'assistants',
+					  'posts_per_page' => -1,	
+					  'meta_key' => 'last_name',
+					  'orderby' => 'meta_value',
+					  'order' => 'ASC',
+					  'tax_query'   => array(
+						array(
+							'taxonomy' => 'departments',
+							'field'    => 'name',
+							'terms'    => $tax->name
+						)
+					  )
+					 );
+					$getassistants = new WP_Query( $argsAssist );				
+					if( $getassistants->have_posts() ) :
+					?>
+					<h1 class="archive-title heading-underline mt-5 mb-4">Assistants</h1>
+					<div class="row UCFassistants">   
+						<?php while( $getassistants->have_posts() ) :
+						  $getassistants->the_post();
+						  $getAssistimageURL = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); ?>
+						<div class="col-md-3 col-sm-6 col-xs-6 col-6 mb-4">
+							<div class="col-10 col-xs-8 col-sm-10 p-0 mb-2 media-background-container UCFassistants-photo mx-auto">
+								<?php if ( has_post_thumbnail()) { ?>
+									<img src="<?php echo $getAssistimageURL; ?>" alt="<?php the_title(); ?>'s profile picture at UCF" title="<?php the_title(); ?>" class="media-background object-fit-cover">
+									<?php } else { ?> 
+											<img src="<?php the_field('default_profile_image', 'option'); ?>" alt="<?php the_title(); ?>'s profile picture at UCF" title="<?php $post->post_title; ?>" class="media-background object-fit-cover">
+								<?php } ?>
+							</div>
+							<strong><?php the_title(); ?></strong></br>
+								<?php the_field('jobtitle'); ?>
+							<div class="mt-2">
+								<?php if(get_field('email')){ ?>
+									<div class="person-label">
+										<a href="mailto:<?php the_field('email'); ?>"><i class="fa fa-envelope iconlink"></i></a>
+									</div>
+								<?php }	?>
+							</div>
+						</div>
+						<?php endwhile;
+						  wp_reset_postdata(); ?>
+					</div>
+					<?php endif; ?>
+<!-- END PART TIME FACULTY -->
+					<?php //wpbeginner_numeric_posts_nav(); ?>
+				</div>
 			</div>
 			<div class="col-lg-3 profilesidebar">
 				<?php dynamic_sidebar( 'directory-sidebar' ); ?>
