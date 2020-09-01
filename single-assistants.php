dae305f64940564880c18d3aff62bb73e3a44dcd
cohpa <djanosik@ucf.edu>
2020-09-01 08:16:19 -0400

    Revert

--- single-assistants.php
+++ single-assistants.php
@@ -17,7 +17,7 @@ $ids = get_the_ID();
 			<div class="col-md-4 mb-5">
 				<aside class="person-contact-container">
 					<div class="mb-4" style="text-align: center;">
-<div class="media-background-container UCFassistants-LGphoto mx-auto">
+<div class="media-background-container UCFassistants-photo mx-auto">
 	<?php if ( has_post_thumbnail()) { ?>
 		<a href="<?php echo $getimageURL ?>" rel="lightbox" title ="<?php echo get_person_name( $post ); ?> at UCF" ><img width="100%" alt="<?php echo get_person_name( $post ); ?>'s profile picture at UCF" src="<?php echo $getimageURL ?>" class="media-background object-fit-cover"/></a>
 		<?php } else { ?> 
@@ -104,6 +104,14 @@ $ids = get_the_ID();
 <?php }
 ?>
 <style>
+.profileIMG {
+	text-align: center;
+	margin: 0px auto;
+	height: 150px;
+	width: 150px;
+	border-radius: 50% !important;
+	/**box-shadow: 0 0 5px rgba(0,0,0,.1); */
+}
 .site-header .container h1 {
 	display: none !important;
 }	
