c6d152e77825943f47aa55af5f694c28c82530df
ucfchpsweb <djanosik@ucf.edu>
2020-09-14 11:25:57 -0400

    Datetime

--- shortcodes.php
+++ shortcodes.php
@@ -612,8 +612,8 @@ $args = array(
 		'order' => 'DESC', 
 		'meta_query' => array(
         	'meta_key' => 'grant_end_date',
-        	'type' => 'DATE',  // You can also try changing it to TIME or DATE if it doesn't work
-        	'meta_value' => date( "Y-m-d" ),
+        	'type' => 'DATETIME',  // You can also try changing it to TIME or DATE if it doesn't work
+        	'meta_value' => date( "m/d/Y" ),
         	'meta_compare' => '>',
     	),
         'tax_query' => array(
@@ -641,7 +641,7 @@ $loop = new WP_Query($args);
 while($loop->have_posts()) : $loop->the_post(); 
 ?>
 <div class="pt-3 pb-3 grantResult <?php echo $g['style']; ?>">
-HELLO<?php get_template_part( 'grant-results'); ?>
+<?php get_template_part( 'grant-results'); ?>
 </div>
 <?php endwhile; ?>
 <div class="mt-5">
