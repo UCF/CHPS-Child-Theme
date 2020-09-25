72299d8c1203fb78000aad84b824b0370f772194
ucfchpsweb <djanosik@ucf.edu>
2020-09-25 15:30:28 -0400

    Update shortcodes.php
    new class

--- shortcodes.php
+++ shortcodes.php
@@ -108,7 +108,7 @@ while($visualnews->have_posts()) : $visualnews->the_post();
 			} else { 
 				 $listnews .= '<a href="' . get_the_permalink() . '" rel="bookmark" title="' . get_the_title() . '">';
 			}
-		$listnews .= '<div class="visnews"><div class="media-background-container visnews-photo mx-auto">';
+		$listnews .= '<div class="visnews"><div class="media-background-container listpersonG-pic mx-auto">';
 	} else {	
 		$listnews .= '<div class="col-lg-3 col-sm-6 col-xs-12">';
 			if ( get_field( 'updatenewstype' ) == 1 ) { 
@@ -116,7 +116,7 @@ while($visualnews->have_posts()) : $visualnews->the_post();
 			} else { 
 				 $listnews .= '<a href="' . get_the_permalink() . '" rel="bookmark" title="' . get_the_title() . '">';
 			}
-		$listnews .= '<div class="visnews"><div class="media-background-container visnews-photo mx-auto">';
+		$listnews .= '<div class="visnews"><div class="media-background-container listpersonG-pic mx-auto">';
 	}	
 	if ( has_post_thumbnail()) {	
 		$listnews .= '<img src="' . $getimgURL . '" alt="' . get_the_title() . '" title="' . get_the_title() . '" class="media-background object-fit-cover">';
@@ -734,7 +734,9 @@ while($visualnews->have_posts()) : $visualnews->the_post();
 	}	
 	$listnews .= '</div><div class="p-3">';
 	$listnews .= '<strong>' . get_the_title() . '</strong>';
-	
+	//INSERT DEGREES
+	//
+	//END DEGREES
 	if ( !empty($a['showjob'])) {
 			if(get_field('job_titles_tax', $post->ID)){	
 			$listnews .= '<br>';
