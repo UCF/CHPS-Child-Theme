03169175ff899fbe1889d756181db8faa9abaef6
ucfchpsweb <djanosik@ucf.edu>
2020-09-28 10:33:38 -0400

    Update shortcodes.php
    li

--- shortcodes.php
+++ shortcodes.php
@@ -733,11 +733,8 @@ if( get_field('degrees', $post->ID) ) {
 			  if (!get_sub_field('degree_aftername', $post->ID)) {
 				continue;
 			  }
-		 $array[] = get_sub_field('degree_select'); 
-		
-			$foo = implode(', ', array_column($array, 'label'));
-			$listnews .= '<span class"">, ' . $foo . '</span>';
-			endwhile;
+		 $listnews .= '<li>' . get_sub_field('degree_select', $post->ID) . '</li>';
+		endwhile;
 		}
 	//END DEGREES
 	if ( !empty($a['showjob'])) {
