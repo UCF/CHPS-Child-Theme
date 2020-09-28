e87d46b417f55e4ed44f5efeaf560f7dd1b1414e
ucfchpsweb <djanosik@ucf.edu>
2020-09-28 14:35:46 -0400

    Update shortcodes.php
    id

--- shortcodes.php
+++ shortcodes.php
@@ -738,7 +738,7 @@ if( get_field('degrees', $post->ID) ) {
 		 $listnews .= ', ' . $value . '';
 		 
 		 
-		 $array[] = get_sub_field_object('degree_select');
+		 $array[] = get_sub_field_object('degree_select', $post->ID);
 		endwhile;
 			$foo = implode(', ', array_column($array, 'label'));
 			$listnews .= '<span class"">, ' . $foo . '</span>';
