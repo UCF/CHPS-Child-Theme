4461baf00707c58995b46a581c11993732b4f478
ucfchpsweb <djanosik@ucf.edu>
2020-10-08 15:29:17 -0400

    Update archive-lab.php
    title

--- archive-lab.php
+++ archive-lab.php
@@ -18,7 +18,7 @@
 						<div class="col-xl-10">
 							<div class="header-title-wrapper">
 								<h1 class="header-title">CHPS Research Labs</h1>
-								<p class="header-subtitle">UCF College of Health Professions and Sciences</p>
+								<p class="header-subtitle">UCF <?php echo post_type_archive_title( '', false );?></p>
 							</div>
 						</div>
 					</div>
