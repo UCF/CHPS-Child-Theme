8b6d50aa999f7b32b66c3a8db299cad383448752
ucfchpsweb <djanosik@ucf.edu>
2020-11-04 16:40:26 -0500

    Update single-person.php
    fa-file-text

--- single-person.php
+++ single-person.php
@@ -77,7 +77,7 @@ $ids = get_the_ID();
                     <?php if (get_field('cv')): ?>	
 						<div class="row mt-3 mb-5">
 							<div class="col-md offset-md-0 col-8 offset-2 my-1">
-								<a href="<?php the_field('cv'); ?>" title="View <?php echo get_person_name( $post ); ?>'s Curriculum Vitae" target="_blank" class="btn btn-primary btn-block"><i class="vc_btn3-icon fa fa-bookmark" style="font-size: 20px; height: 16px; line-height: 16px; margin-right: 15px;"></i> View Curriculum Vitae</a>
+								<a href="<?php the_field('cv'); ?>" title="View <?php echo get_person_name( $post ); ?>'s Curriculum Vitae" target="_blank" class="btn btn-primary btn-block"><i class="vc_btn3-icon fa fa-file-text" style="font-size: 20px; height: 16px; line-height: 16px; margin-right: 15px;"></i> View Curriculum Vitae</a>
 							</div>
 						</div>
 					<?php endif; ?>		
