09cdf7862f552f4a3ba127d83239e9c9b968b3e5
ucfchpsweb <djanosik@ucf.edu>
2020-11-04 17:17:38 -0500

    Update single-person.php
    mb1

--- single-person.php
+++ single-person.php
@@ -68,7 +68,7 @@ $ids = get_the_ID();
 						<?php }	?>
 					</div>
 					<?php if (get_field('google_scholar')): ?>	
-						<div class="row mt-3 mb-2">
+						<div class="row mt-3 mb-1">
 							<div class="col-md offset-md-0 col-8 offset-2 my-1">
 								<a href="<?php the_field('google_scholar'); ?>" title="View <?php echo get_person_name( $post ); ?>'s Publications" target="_blank" class="btn btn-primary btn-block"><i class="vc_btn3-icon fa fa-bookmark" style="font-size: 20px; height: 16px; line-height: 16px; margin-right: 15px;"></i> View Publications</a>
 							</div>
