0cad955f90b6c260d291700d69a859876ebda320
ucfchpsweb <djanosik@ucf.edu>
2020-12-02 11:41:37 -0500

    Update functions.php
    revert

--- functions.php
+++ functions.php
@@ -218,6 +218,128 @@ if( function_exists('acf_add_options_page') ) {
 
 
 
+function wpbeginner_numeric_posts_nav() {
+ 
+    if( is_singular() )
+        return;
+ 
+    global $wp_query;
+ 
+    /** Stop execution if there's only 1 page */
+    if( $wp_query->max_num_pages <= 1 )
+        return;
+ 
+    $paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
+    $max   = intval( $wp_query->max_num_pages );
+ 
+    /** Add current page to the array */
+    if ( $paged >= 1 )
+        $links[] = $paged;
+ 
+    /** Add the pages around the current page to the array */
+    if ( $paged >= 3 ) {
+        $links[] = $paged - 1;
+        $links[] = $paged - 2;
+    }
+ 
+    if ( ( $paged + 2 ) <= $max ) {
+        $links[] = $paged + 2;
+        $links[] = $paged + 1;
+    }
+ 
+    echo '<div class="navigation"><ul>' . "\n";
+ 
+    /** Previous Post Link */
+    if ( get_previous_posts_link() )
+        printf( '<li>%s</li>' . "\n", get_previous_posts_link() );
+ 
+    /** Link to first page, plus ellipses if necessary */
+    if ( ! in_array( 1, $links ) ) {
+        $class = 1 == $paged ? ' class="active"' : '';
+ 
+        printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( 1 ) ), '1' );
+ 
+        if ( ! in_array( 2, $links ) )
+            echo '<li>…</li>';
+    }
+ 
+    /** Link to current page, plus 2 pages in either direction if necessary */
+    sort( $links );
+    foreach ( (array) $links as $link ) {
+        $class = $paged == $link ? ' class="active"' : '';
+        printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $link ) ), $link );
+    }
+ 
+    /** Link to last page, plus ellipses if necessary */
+    if ( ! in_array( $max, $links ) ) {
+        if ( ! in_array( $max - 1, $links ) )
+            echo '<li>…</li>' . "\n";
+ 
+        $class = $paged == $max ? ' class="active"' : '';
+        printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $max ) ), $max );
+    }
+ 
+    /** Next Post Link */
+    if ( get_next_posts_link() )
+        printf( '<li>%s</li>' . "\n", get_next_posts_link() );
+ 
+    echo '</ul></div>' . "\n";
+ 
+}
+function mychildtheme_person_post_type_args( $args ) {
+	$args['rewrite'] = array( 'with_front' => false );
+	return $args;
+}
+add_filter( 'ucf_people_post_type_args', 'mychildtheme_person_post_type_args', 10, 1 );
 
 
+// TRYING TO FIGURE THIS OUT
+function get_header_markup_dave() {
+	$videos = $images = null;
+	$obj = get_queried_object();
+
+	if ( is_single() || is_page() ) {
+		$videos = get_header_videos( $obj );
+		$images = get_header_images( $obj );
+	}
+
+	echo get_nav_markup(); ?>
+<?php 
+if ( get_field( 'breadcrumb', 'option' ) == 1 ) { ?>
+<div class="breadcrumbnav">
+	<div class="container">
+		<a href="/" title="UCF College of Health Professions and Sciences" class="yellow">College of Health Professions and Sciences</a><span class="hidemobile"> > <a href="<?php echo get_site_url(); ?>" title="<?php bloginfo( 'name' ); ?> at the UCF College of Health Professions and Sciences"><?php bloginfo( 'name' ); ?></a>
+       	<?php if ( !is_front_page() ) { ?>
+         	> <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?> at the UCF <?php bloginfo( 'name' ); ?>"><?php the_title(); ?></a></span>
+    	<?php } ?>
+    </div>
+</div>
+<?php } ?>
+<?php 
+switch_to_blog(2);
+$hero = get_field('top_alert', 'option');
+?>
+<style>
+.topAlert a {
+	color: <?php echo $hero['textcolor']; ?> !important;
+	text-decoration:underline;
+}
+</style>
+<?php
+if ( $hero['activation'] == 1 ) { ?>
+<div class="topAlert" style="background-color:<?php echo $hero['bgcolor']; ?>; color:<?php echo $hero['textcolor']; ?>;">
+	<div class="container">
+		<i class="<?php echo $hero['icon']; ?>"></i> <?php echo $hero['message']; ?>
+    </div>
+</div>
+<?php } ?>
+<?php restore_current_blog(); ?>
+<?php 
+	if ( $videos || $images ) {
+		echo get_header_media_markup( $obj, $videos, $images );
+	}
+	else {
+		echo get_header_default_markup( $obj );
+	}
+}
 ?>
