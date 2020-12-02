86cc23ec287dd8c49b7ea78f7cba37a3a7d97cf4
ucfchpsweb <djanosik@ucf.edu>
2020-12-02 12:07:20 -0500

    css

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php get_header();?>
 
+<style>	
+header .container h1 {
+	display: none !important;
+}		
+.authortext {
+	text-transform: uppercase;
+	font-size: 12px;
+	color: #777;
+	margin-top: 20px;
+}
+h3.widget-title {
+	font-size: 18px;
+}
+h2 {
+	text-transform: none !important;
+}	
+div.widget-content ul {
+	margin-left: 10px;
+	padding-left: 0px;
+}
+div.widget-content ul li {
+	list-style: none;
+	border-bottom: 1px solid #eee;
+	padding:6px 0px;
+	font-size: 14px !important;
+}
+</style>
 <?php get_footer(); ?>
