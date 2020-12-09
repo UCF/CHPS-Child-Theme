5e731a510bfcd5d8d54c62d9b3659830c62d88f2
ucfchpsweb <djanosik@ucf.edu>
2020-12-09 13:26:50 -0500

    Update taxonomy-departments.php
    emeritus

--- taxonomy-departments.php
+++ taxonomy-departments.php
@@ -106,11 +106,7 @@ $tax = $wp_query->get_queried_object();
 						<?php while( $getassistants->have_posts() ) :
 						  $getassistants->the_post();
 						  $getAssistimageURL = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); ?>
-                          
-                         
-                        
                                 <div class="w-20 mb-4">
-                        
                                     <div class="p-0 mb-2 media-background-container UCFassistants-photo mx-auto">
                                     <?php if ( get_the_content() ) { ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>'s Profile" ><?php } ?>
 										<?php if ( has_post_thumbnail()) { ?>
@@ -146,6 +142,50 @@ $tax = $wp_query->get_queried_object();
 <!-- END PART TIME FACULTY -->
 					<?php //wpbeginner_numeric_posts_nav(); ?>
 				</div>
+				<!-- then the pagination links -->
+				<div class="mt-5 pt-3">
+				<!-- START EMERITUS FACULTY -->
+					<?php
+					$argsEmeritus = array(
+					  'post_type'   => 'emeritus',
+					  'posts_per_page' => -1,	
+					  'meta_key' => 'lname',
+					  'orderby' => 'meta_value',
+					  'order' => 'ASC',
+					  'tax_query'   => array(
+						array(
+							'taxonomy' => 'departments',
+							'field'    => 'name',
+							'terms'    => $tax->name
+						)
+					  )
+					 );
+					$emeritus = new WP_Query( $argsEmeritus );				
+					if( $emeritus->have_posts() ) :
+					?>
+					<h1 class="archive-title heading-underline mt-5 mb-4">Emeritus Faculty</h1>
+					<div class="row parttimers">   
+						<?php while( $emeritus->have_posts() ) :
+						  $emeritus->the_post();
+						  $getPTimageURL = wp_get_attachment_url( get_post_thumbnail_id($post->ID), 'large' ); ?>
+						<div class="col-md-3 col-sm-6 col-xs-6 col-6 mb-4">
+							<div class="col-10 col-xs-8 col-sm-10 p-0 mb-2 media-background-container parttime-photo mx-auto">
+								<?php if ( has_post_thumbnail()) { ?>
+									<img src="<?php echo $getPTimageURL; ?>" alt="<?php the_title(); ?>'s profile picture at UCF" title="<?php the_title(); ?>" class="media-background object-fit-cover">
+									<?php } else { ?> 
+											<img src="<?php the_field('default_profile_image', 'option'); ?>" alt="<?php the_title(); ?>'s profile picture at UCF" title="<?php $post->post_title; ?>" class="media-background object-fit-cover">
+								<?php } ?>
+							</div>
+							<strong><?php the_title(); ?></strong></br>
+								<?php the_field('jobtitle'); ?>
+						</div>
+						<?php endwhile;
+						  wp_reset_postdata(); ?>
+					</div>
+					<?php endif; ?>
+<!-- END EMERITUS FACULTY -->
+					<?php //wpbeginner_numeric_posts_nav(); ?>
+				</div>                
 			</div>
 			<div class="col-lg-3 profilesidebar">
 				<?php dynamic_sidebar( 'directory-sidebar' ); ?>
