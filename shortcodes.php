cac9fbb5157c3b1b3696085c771eacaee745e4a2
cohpa <djanosik@ucf.edu>
2021-03-26 13:53:19 -0400

    clear 2

--- shortcodes.php
+++ shortcodes.php
@@ -844,7 +844,7 @@ if ( !empty($a['showunit'])) { }
 	}
 $listlabs .= '<h4>';	
 if( get_field('website_url') ) {		
-	$listlabs .= '<a href="' . get_field('website_url', $post->ID) . '" target="_blank" rel="noopener noreferrer nofollow external" data-wpel-link="external">';
+	$listlabs .= '<a href="' . get_field('website_url') . '" target="_blank" rel="noopener noreferrer nofollow external" data-wpel-link="external">';
 	}
 $listlabs .= get_the_title();
 if( get_field('website_url') ) {	
@@ -853,7 +853,7 @@ if( get_field('website_url') ) {
 $listlabs .= '</h4>';
 $listlabs .= $content;
 if( get_field('website_url') ) {	
-	$listlabs .= '<div class="vc_btn3-container  btnhover-yellow vc_btn3-left mt-3" ><a style="background-color:#ffcc00; color:#000000;" class="vc_general vc_btn3 vc_btn3-size-md vc_btn3-shape-square vc_btn3-style-custom vc_btn3-icon-left" href="' . get_field('website_url', $post->ID) . '" title="" target="_blank" rel="nofollow external noopener noreferrer" data-wpel-link="external"><i class="vc_btn3-icon fa fa-external-link"></i> Visit the Lab&apos;s Website</a></div>';
+	$listlabs .= '<div class="vc_btn3-container  btnhover-yellow vc_btn3-left mt-3" ><a style="background-color:#ffcc00; color:#000000;" class="vc_general vc_btn3 vc_btn3-size-md vc_btn3-shape-square vc_btn3-style-custom vc_btn3-icon-left" href="' . get_field('website_url') . '" title="" target="_blank" rel="nofollow external noopener noreferrer" data-wpel-link="external"><i class="vc_btn3-icon fa fa-external-link"></i> Visit the Lab&apos;s Website</a></div>';
 }
 $listlabs .= '</div></div></div>';
 endwhile;
