cac0ad892fa62ba48955aeaca2b337f4a5b8671e
cohpa <djanosik@ucf.edu>
2021-03-26 13:37:33 -0400

    clear

--- shortcodes.php
+++ shortcodes.php
@@ -765,11 +765,7 @@ if( get_field('degrees') ) {
 		}
 	}
 //TESTING ENDS HERE		
-$post_categories = get_the_terms( $post->ID, 'job_titles' );
-if ( ! empty( $post_categories ) && ! is_wp_error( $post_categories ) ) {
-    $categories = wp_list_pluck( $post_categories, 'name' );
-}
-$listnews .= $categories;	
+
 //TESTING ENDS HERE	
 	$listnews .= '</div></div></a></div>';				
 endwhile;
