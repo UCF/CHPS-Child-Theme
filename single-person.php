00a0242cf45a094b4c3c58d42a82f56b3eb2b1e2
cohpa <djanosik@ucf.edu>
2021-12-16 12:25:57 -0500

    LIKE

--- single-person.php
+++ single-person.php
@@ -232,7 +232,7 @@ $posts = get_posts(array(
 		array(  
 			'key' => 'tag_person', // slug of custom field
 			'value' => $ids, // keep this to match current profile
-			'compare' => 'IN'
+			'compare' => 'LIKE'
 			  )
 		 )
 ));
