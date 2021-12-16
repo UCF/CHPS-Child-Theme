ecfda787dd9cfd5a64e20ad14b87ceaccf5dff66
cohpa <djanosik@ucf.edu>
2021-12-16 12:54:00 -0500

    number

--- single-person.php
+++ single-person.php
@@ -232,7 +232,7 @@ $posts = get_posts(array(
 	'meta_query' => array(
 		array(  
 			'key' => 'tag_person', // slug of custom field
-			'value' => $ids, // keep this to match current profile
+			'value' => array('18461'), // keep this to match current profile
 			'compare' => 'IN'
 			  )
 		 )
