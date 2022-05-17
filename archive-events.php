254015b04769c9d0d69c9159e3908b8bab0c5ef4
cohpa <djanosik@ucf.edu>
2022-05-17 08:58:28 -0400

    Update archive-events.php

--- archive-events.php
+++ archive-events.php
@@ -30,7 +30,7 @@
 		<div class="row">
 			<div class="col-lg-12 col-md-12 pt-5">
 				
-<h3>ATTEND AN ADMISSION EVENT</h3>
+<h3>Attend an Admissions Event</h3>
 <p>The College of Health Professions and Sciences offers a variety of opportunities for a candidate to learn more about UCF and specialized graduate programs. Our admission events listed below provide detailed information about the specific program including curriculum, learning outcomes, program format and schedule, admission requirements, application process, as well as an opportunity to visit the location of the program.</p>
 
 <h4>Upcoming Events</h4>
