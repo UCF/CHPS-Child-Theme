e6cd0ff0e633f94e486506be9a11500fc7aec0ca
cohpa <djanosik@ucf.edu>
2022-05-17 08:16:33 -0400

    Update archive-events.php

--- archive-events.php
+++ archive-events.php
@@ -1,8 +1,8 @@
  <?php get_header(); ?>
 		<div class="header-media  header-media-default media-background-container mb-0 d-flex flex-column">
 			<video class="hidden-xs-down media-background media-background-video object-fit-cover" autoplay muted playsinline loop>
-				<source src="https://healthprofessions.ucf.edu/wp-content/uploads/sites/2/2020/03/grants-3.webm" type="video/webm">
-				<source src="https://healthprofessions.ucf.edu/wp-content/uploads/sites/2/2020/03/grants3.mp4" type="video/mp4">
+				<source src="https://healthprofessions.ucf.edu/wp-content/uploads/sites/2/2022/05/ucf_28926832-compressed.webm" type="video/webm">
+				<source src="https://healthprofessions.ucf.edu/wp-content/uploads/sites/2/2022/05/ucf_28926832-compressed.mp4" type="video/mp4">
 			</video>
 			<button class="media-background-video-toggle btn play-enabled hidden-xs-up" type="button" data-toggle="button" aria-pressed="false" aria-label="Play or pause background videos">
 				<span class="fa fa-pause media-background-video-pause" aria-hidden="true"></span>
