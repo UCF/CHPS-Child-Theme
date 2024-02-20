08a2922846a0b9c33862ece6e873f6166c8fdd5b
David Janosik <cohpa@users.noreply.github.com>
2024-02-20 15:23:57 -0500

    aria-label

--- taxonomy-departments.php
+++ taxonomy-departments.php
@@ -125,12 +125,12 @@ $tax = $wp_query->get_queried_object();
                                     <div class="mt-2">
                                         
                                             <div class="person-label">
-                                                <?php if(get_field('email')){ ?><a href="mailto:<?php the_field('email'); ?>" alt="<?php the_title_attribute(); ?>'s Email"><i class="fa fa-envelope iconlink"></i></a><?php }?>
-                                                <?php if(get_field('research_link')){ ?><a href="<?php the_field('research_link'); ?>" alt="<?php the_title_attribute(); ?>'s Research" target="_blank"><i class="fa fa-file-text iconlink"></i></a><?php }?>
-                                                <?php if(get_field('linkedin_link')){ ?><a href="<?php the_field('linkedin_link'); ?>" alt="<?php the_title_attribute(); ?>'s LinkedIn" target="_blank"><i class="fa fa-linkedin-square iconlink"></i></a><?php }?>
-                                                <?php if(get_field('facebook_link')){ ?><a href="<?php the_field('facebook_link'); ?>" alt="<?php the_title_attribute(); ?>'s Facebook" target="_blank"><i class="fa fa-facebook-square iconlink"></i></a><?php }?>
-                                                <?php if(get_field('twitter_link')){ ?><a href="<?php the_field('twitter_link'); ?>" alt="<?php the_title_attribute(); ?>'s X" target="_blank"><i class="fa fa-twitter-square iconlink"></i></a><?php }?>
-                                                <?php if(get_field('instagram_link')){ ?><a href="<?php the_field('instagram_link'); ?>" alt="<?php the_title_attribute(); ?>'s Instagram" target="_blank"><i class="fa fa-instagram iconlink"></i></a><?php }?>
+                                                <?php if(get_field('email')){ ?><a href="mailto:<?php the_field('email'); ?>" aria-label="<?php the_title_attribute(); ?>'s Email"><i class="fa fa-envelope iconlink"></i></a><?php }?>
+                                                <?php if(get_field('research_link')){ ?><a href="<?php the_field('research_link'); ?>" aria-label="<?php the_title_attribute(); ?>'s Research" target="_blank"><i class="fa fa-file-text iconlink"></i></a><?php }?>
+                                                <?php if(get_field('linkedin_link')){ ?><a href="<?php the_field('linkedin_link'); ?>" aria-label="<?php the_title_attribute(); ?>'s LinkedIn" target="_blank"><i class="fa fa-linkedin-square iconlink"></i></a><?php }?>
+                                                <?php if(get_field('facebook_link')){ ?><a href="<?php the_field('facebook_link'); ?>" aria-label="<?php the_title_attribute(); ?>'s Facebook" target="_blank"><i class="fa fa-facebook-square iconlink"></i></a><?php }?>
+                                                <?php if(get_field('twitter_link')){ ?><a href="<?php the_field('twitter_link'); ?>" aria-label="<?php the_title_attribute(); ?>'s X" target="_blank"><i class="fa fa-twitter-square iconlink"></i></a><?php }?>
+                                                <?php if(get_field('instagram_link')){ ?><a href="<?php the_field('instagram_link'); ?>" aria-label="<?php the_title_attribute(); ?>'s Instagram" target="_blank"><i class="fa fa-instagram iconlink"></i></a><?php }?>
                                             </div>
                                         
                                     </div>
