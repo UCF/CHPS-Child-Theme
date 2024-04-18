cc912541e2e21bcf35201b5f1996a1ea4242f4aa
David Janosik <cohpa@users.noreply.github.com>
2024-04-18 17:06:07 -0400

    img

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@
 			<div class="col-md-2">
 			</div>
 			<div class="col-md-3">
+				<img src="https://healthprofessions.ucf.edu/wp-content/uploads/sites/2/2024/04/ucf-GiftBox.jpg" width="300" alt="Gift box icon">
 			</div>
 			<div class="col-md-5">
 				<h2>GIVE A GIFT</h2>
