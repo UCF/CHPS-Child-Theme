63d6ebfef47c3c36b1115303877c8f5f95f5b855
David Janosik <cohpa@users.noreply.github.com>
2024-04-30 14:42:34 -0400

    array

--- single-person.php
+++ single-person.php
@@ -452,7 +452,14 @@ add_filter('posts_where', 'my_posts_where');
 <?php 
 $posts = get_posts(array(
     'posts_per_page'    => 7,
-    'post_type'         => 'post'
+    'post_type'         => 'post',
+	'meta_query' => array(
+		array(  
+			'key' => 'tag_person', // slug of custom field
+			'value' => $ids, // keep this to match current profile
+			'compare' => '='
+			  )
+		 )
 ));
 if( $posts ): ?>
     <ul>
