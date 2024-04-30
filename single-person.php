9136e20f2f3190c4108b0849a16c4b560cc57575
David Janosik <cohpa@users.noreply.github.com>
2024-04-30 16:01:46 -0400

    dr

--- single-person.php
+++ single-person.php
@@ -232,8 +232,8 @@ $peepargs  = get_posts(array(
 	'meta_query'    => array(
         array(
             'key'       => 'tag_person',
-            'value'     => array( 22, 4 ),
-            'compare'   => 'IN'
+            'value'     => '22',
+            'compare'   => 'LIKE'
         ),
     )
 ));					
@@ -444,6 +444,49 @@ add_filter('posts_where', 'my_posts_where');
 					</div></div>
 					<?php endif; ?>
 				</section>
+			
+			
+			
+			
+			
+			 <?php 
+
+                        /*
+                        *  Query posts for a relationship value.
+                        *  This method uses the meta_query LIKE to match the string "123" to the database value a:1:{i:0;s:3:"123";} (serialized array)
+                        */
+
+                        $doctors = get_posts(array(
+                            'post_type' => 'posts',
+                            'meta_query' => array(
+                                array(
+                                    'key' => 'tag_person', // name of custom field
+                                    'value' => '"' . get_the_ID() . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
+                                    'compare' => 'LIKE'
+                                )
+                            )
+                        ));
+ ?>
+                        <?php if( $doctors ): ?>
+                            <ul>
+                            <?php foreach( $doctors as $doctor ): ?>
+                                <li>
+                                    <a href="<?php echo get_permalink( $doctor->ID ); ?>">
+                                        <img src="<?php echo $photo['url']; ?>" alt="<?php echo $photo['alt']; ?>" width="30" />
+                                        <?php echo get_the_title( $doctor->ID ); ?>
+                                    </a>
+                                </li>
+                            <?php endforeach; ?>
+                            </ul>
+                        <?php endif; ?>
+			
+			
+			
+			
+			
+			
+			
+			
 			</div>
 		</div>
 			<script>
